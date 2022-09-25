62e122826ee775fe5c4f96febd147af9a0ae1279
Marius Jagminas <asmarenas@gmail.com>
2022-09-25 16:10:23 +0200

    Update readme

--- README.md
+++ README.md
@@ -5,3 +5,11 @@ Deploy
 ```
 npx hardhat run scripts/deploy.ts --network goerli
 ```
+
+Contract address:
+
+```
+0x95805A73ef14F12574FE4711d94CAbA0692Df47e
+```
+
+[Etherscan link](https://goerli.etherscan.io/address/0x95805A73ef14F12574FE4711d94CAbA0692Df47e)
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -5,7 +5,7 @@ async function main() {
   console.log("Deploying contracts with the account:", deployer.address);
   console.log("Account balance:", (await deployer.getBalance()).toString());
 
-  const ContractFactory = await ethers.getContractFactory("ContractMJ");
+  const ContractFactory = await ethers.getContractFactory("Token");
   const contract = await ContractFactory.deploy("MM", "MM");
 
   await contract.deployed();
