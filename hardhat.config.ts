870a38b35a62534a938147acc8cf978736e31d7d
Marius Jagminas <asmarenas@gmail.com>
2022-10-09 16:39:07 +0200

    Change hardhat.config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,14 +1,17 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import { readFileSync } from "fs";
 
-const API_KEY = process.env.API_KEY;
-const PRIVATE_KEY =
-  process.env.PRIVATE_KEY ||
-  "1111111111111111111111111111111111111111111111111111111111111111";
+const secrets = JSON.parse(readFileSync("secrets.json").toString());
+
+const API_KEY = secrets["infura_api_key"];
+const PRIVATE_KEY = secrets["accounts"]["goerli_network"];
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  defaultNetwork: "hardhat",
   networks: {
+    hardhat: {},
     goerli: {
       url: `https://goerli.infura.io/v3/${API_KEY}`,
       accounts: [PRIVATE_KEY],
